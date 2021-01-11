b341890b3d35eb322ca6a372703e083de319e0bd
Anuja Agarwal <anujagrazzel@gmail.com>
2021-01-11 18:23:33 +0530

    Update Feature_APIs.py

--- Feature_APIs.py
+++ Feature_APIs.py
@@ -1,4 +1,4 @@
-from elasticsearch import Elasticsearch,NotFoundError,RequestError
+from elasticsearch import Elasticsearch, NotFoundError, RequestError
 from flask import Flask, jsonify, request, Blueprint
 from elasticsearch import helpers
 import re
@@ -17,9 +17,11 @@ es = Elasticsearch()
 @app.route('/create_index', methods=['POST'])
 def create_index():
     requestf = request.get_json()              # extracted the request body
-    indexname = requestf["indexname"]          # extracted the indexname from the request body
+    # extracted the indexname from the request body
+    indexname = requestf["indexname"]
     synonym_path = requestf["synonym_path"]
-    mapping = requestf["mapping_property"]     # extracted mapping from the request body
+    # extracted mapping from the request body
+    mapping = requestf["mapping_property"]
     request_body = {                           # settings which need to be performed is stored in this variable
         "settings": {
             "index": {
@@ -63,25 +65,29 @@ def create_index():
         es.indices.create(index=indexname, body=request_body)
         res = es.transport.perform_request(
             'PUT', '/'+indexname+'/_mappings', body={"properties": mapping})
-        return {"message":"success","response":res},"200"
+        return {"message": "success", "response": res}, "200"
     except RequestError as es1:
-        return es1.info,es1.status_code
+        return es1.info, es1.status_code
 
 # autocomplete
+
+
 @app.route('/<indexname>/autocomplete', methods=["POST"])
-def autocomplete_size(indexname):                                 
-    response = request.get_json()                          # extracted the request body
-    input = response["input"]                              # extracted the input from the request body
+def autocomplete_size(indexname):
+    # extracted the request body
+    response = request.get_json()
+    # extracted the input from the request body
+    input = response["input"]
     pattern = re.compile('\W')
-    sanitized_input = re.sub(pattern, ' ', input)           # input is passed through sanitization
+    # input is passed through sanitization
+    sanitized_input = re.sub(pattern, ' ', input)
     if "size" in response.keys():
-        size = response["size"] 
+        size = response["size"]
         if size <= 0:
-            return {"message":"Size should be greater than 0"},400
+            return {"message": "Size should be greater than 0"}, 400
     else:
         size = 10
     try:
-            
         res = es.search(index=indexname, body={"query": {
             "query_string": {
                 "query": sanitized_input}
@@ -95,29 +101,37 @@ def autocomplete_size(indexname):
                     "terms": {"field": "category_name"}
                 }
         }, "size": size})
-        res["by_brand"] = res["aggregations"]["by_brand"]["buckets"]   
+        res["by_brand"] = res["aggregations"]["by_brand"]["buckets"]
         res["by_category"] = res["aggregations"]["by_category"]["buckets"]
         return res
     except NotFoundError as es1:
-        return es1.info,es1.status_code
+        return es1.info, es1.status_code
 
 # search
+
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    response = request.get_json()                          # extracted the request body
-    indexname = response["indexname"]                      # extracted the indexname from the request body
-    size = response["size"]                                # extracted the size from the request body
-    input = response["input"]                              # extracted the input from the request body
+    # extracted the request body
+    response = request.get_json()
+    # extracted the indexname from the request body
+    indexname = response["indexname"]
+    # extracted the size from the request body
+    size = response["size"]
+    # extracted the input from the request body
+    input = response["input"]
     From = response["From"]
-    
-    range_fields_value = response["range_fields_value"]    # extracted dictionary of range fields and its values
-    field_terms = response["filter_terms"]                 # extracted dictionary of term fields and its values
+
+    # extracted dictionary of range fields and its values
+    range_fields_value = response["range_fields_value"]
+    # extracted dictionary of term fields and its values
+    field_terms = response["filter_terms"]
     filter_list = []                                       # initialized empty list
     for field in field_terms.keys():
         if field == "category":
-            return {"message":"Try 'category_name' instead of 'category'","status_code":"400"},400
+            return {"message": "Try 'category_name' instead of 'category'", "status_code": "400"}, 400
         elif field == "brand":
-            return {"message":"Try 'brand_name' instead of 'brand'","status_code":"400"},400
+            return {"message": "Try 'brand_name' instead of 'brand'", "status_code": "400"}, 400
         else:
             filter_list.append({"terms": {field: field_terms[field]}})
     for range_field in range_fields_value.keys():
@@ -125,10 +139,12 @@ def search():
                            "gte": range_fields_value[range_field]["min_val"], "lte": range_fields_value[range_field]["max_val"]}}})
         print(
             {"range": {range_field: {"gte": range_fields_value[range_field]["min_val"], "lte": range_fields_value[range_field]["max_val"]}}})
-    sortlist = response["sortlist"]                         # extracted dictionary of sort fields and its sorting type
-    search_fields = response["search_fields"]               # extracted fields based on which search will be performed
+    # extracted dictionary of sort fields and its sorting type
+    sortlist = response["sortlist"]
+    # extracted fields based on which search will be performed
+    search_fields = response["search_fields"]
     if input == "*":
-        res_1={}
+        res_1 = {}
         search_res = es.transport.perform_request(
             'POST', '/'+indexname+'/_search', body={"size": size, "from": From, "sort": sortlist, "query": {
                 "function_score": {
@@ -139,8 +155,8 @@ def search():
                         }
                     }
                 }
-            }, 
-            "aggs": {                                                 # Facets feature
+            },
+                "aggs": {                                                 # Facets feature
                 "by_brand": {
                     "terms": {"field": "brand_name"}
                 },
@@ -152,7 +168,7 @@ def search():
             "query": {
                 "bool": {
                     "must": [
-                        {"match": {"bannercheck": "true"}}                    ]
+                        {"match": {"bannercheck": "true"}}]
                 }
             }
         }
@@ -163,12 +179,11 @@ def search():
         res_1["by_brand"] = res_1["search_response"]["aggregations"]["by_brand"]["buckets"]
         res_1["by_category"] = res_1["search_response"]["aggregations"]["by_category"]["buckets"]
         return (res_1)
-
     pattern = re.compile('\W')
-    sanitized_input = re.sub(pattern, ' ', input)           # input went through sanitization
-    print(sanitized_input)
+    # input went through sanitization
+    sanitized_input = re.sub(pattern, ' ', input)
     try:
-            
+
         search_response = es.search(index=indexname, body={
             "sort": sortlist,
             "query": {
@@ -180,8 +195,8 @@ def search():
                                     "multi_match": {
                                         "query": sanitized_input,
                                         "fields": search_fields,
-                                        "type":"cross_fields"
-                                        
+                                        "type": "cross_fields"
+
                                     }
                                 }
                             ],
@@ -217,9 +232,22 @@ def search():
         }
         )
         res = {}
-        if search_response["hits"]["total"]["value"] == 0 and len(search_response["suggest"]["mytermsuggester1"])!=0 and len(search_response["suggest"]["mytermsuggester1"][0]["options"]) == 0:
-            return {"Results": []},404
-        elif search_response["hits"]["total"]["value"] == 0 and len(search_response["suggest"]["mytermsuggester1"])!=0:
+        if search_response["hits"]["total"]["value"] == 0 and len(search_response["suggest"]["mytermsuggester1"]) != 0 and len(search_response["suggest"]["mytermsuggester1"][0]["options"]) == 0:
+            banner_keyword = sanitized_input
+            body = {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {"match": {"bannercheck": "true"}},
+                            {"match": {"banner_keyword": banner_keyword}}
+                        ]
+                    }
+                }
+            }
+            banner_response = es.transport.perform_request(
+                'POST', '/'+indexname+'/_search', body=body)
+            return {"Results": [], "banner_response": banner_response}, 404
+        elif search_response["hits"]["total"]["value"] == 0 and len(search_response["suggest"]["mytermsuggester1"]) != 0:
             suggestWord = search_response["suggest"]["mytermsuggester1"][0]["options"][0]["text"]
             search_response = es.search(index=indexname, body={
                 "sort": sortlist,
@@ -232,8 +260,8 @@ def search():
                                         "multi_match": {
                                             "query": suggestWord,
                                             "fields": search_fields,
-                                            "type":"cross_fields"
-                                        
+                                            "type": "cross_fields"
+
                                         }
                                     }
                                 ],
@@ -289,18 +317,21 @@ def search():
         res["by_category"] = res["search_response"]["aggregations"]["by_category"]["buckets"]
         return (res)
     except NotFoundError as es1:
-        return es1.info,es1.status_code
+        return es1.info, es1.status_code
     except RequestError as es2:
-        return es2.info,es2.status_code
-    
+        return es2.info, es2.status_code
+
 
 # update field by query
 @app.route("/update_field_by_query", methods=['POST'])
 def update_field_with_query():
     requestf = request.get_json()                   # extracted the request body
-    indexname = requestf["indexname"]               # extracted indexname from the request body
-    querylist = requestf["query"]                   # extracted query from the request body
-    updatelist = requestf["update"]                 # extracted updates which need to be performed from the request body
+    # extracted indexname from the request body
+    indexname = requestf["indexname"]
+    # extracted query from the request body
+    querylist = requestf["query"]
+    # extracted updates which need to be performed from the request body
+    updatelist = requestf["update"]
     query = []                                      # initialized empty list
     for queryfield in querylist:
         query.append({"term": {list(queryfield.keys())[0]: {
@@ -329,34 +360,40 @@ def update_field_with_query():
     try:
         res = es.transport.perform_request(
             'POST', '/'+indexname+'/_update_by_query', body=body)
-        if res["updated"]==0:
+        if res["updated"] == 0:
             raise Exception("Doens't exist")
         return res
     except Exception as es1:
-        return {"message":str(querylist)+" Products with the queries doesnt not exist"},404
+        return {"message": str(querylist)+" Products with the queries doesnt not exist"}, 404
 
 
 # bulk update
 @app.route("/bulk_update/<indexname>", methods=['POST'])
 def update(indexname):
-    bulk_data_syno = dict(xmltodict.parse(request.data))                   # converted xml to dictionary
+    # converted xml to dictionary
+    bulk_data_syno = dict(xmltodict.parse(request.data))
     if "products" in bulk_data_syno["root"].keys():
         bulk_data = bulk_data_syno["root"]["products"]["product"]
-        if str(type(bulk_data))=="<class 'dict'>" or str(type(bulk_data))=="<class 'collections.OrderedDict'>":
-            bulk_data=[bulk_data]
+        if str(type(bulk_data)) == "<class 'dict'>" or str(type(bulk_data)) == "<class 'collections.OrderedDict'>":
+            bulk_data = [bulk_data]
         for data in bulk_data:
             data["list_price"] = float(data["list_price"])
-            data["category_name"] = data["category"]                       # extraction of category in to new key
-            data["brand_name"] = data["brand"]                             # extraction of brand in to new key
+            # extraction of category in to new key
+            data["category_name"] = data["category"]
+            # extraction of brand in to new key
+            data["brand_name"] = data["brand"]
             if "seller_product" in data.keys():
-                if str(type(data["seller_product"]["product"]))=="<class 'dict'>" or str(type(data["seller_product"]["product"]))=="<class 'collections.OrderedDict'>":
-                    data["seller_product"]["product"]=[data["seller_product"]["product"]]
+                if str(type(data["seller_product"]["product"])) == "<class 'dict'>" or str(type(data["seller_product"]["product"])) == "<class 'collections.OrderedDict'>":
+                    data["seller_product"]["product"] = [
+                        data["seller_product"]["product"]]
             res = es.index(index=indexname, id=data["id"], body=data)
     if "synonym" in bulk_data_syno["root"].keys():
         synonymlist = bulk_data_syno["root"]["synonym"]["syno"]
+        if str(type(synonymlist)) == "<class 'str'>":
+            synonymlist = [synonymlist]
         try:
             file = open(
-                "C:\\Users\\Win10\\Desktop\\elasticsearch-7.9.1\\config\\analysis\\synonym.txt", "a")
+                "C:\\Users\\91979\\Desktop\\python\\Folder\\elasticsearch-7.9.2-windows-x86_64\\elasticsearch-7.9.2\\config\\analyzers\\synonym.txt", "a")
             file.write("\n")
             file.write("\n".join(synonymlist))
             file.close()
@@ -365,15 +402,16 @@ def update(indexname):
             raise ex
         except IOError as e:
             print("Caught the I/O error.")
-            raise ex
+            raise e
         response = es.transport.perform_request(
             'POST', '/'+indexname+'/_reload_search_analyzers')
     if "banners" in bulk_data_syno["root"].keys():
         bannerlist = bulk_data_syno["root"]["banners"]["banner"]
+        if str(type(bannerlist)) == "<class 'dict'>" or str(type(bannerlist)) == "<class 'collections.OrderedDict'>":
+            bannerlist = [bannerlist]
         for data in bannerlist:
             res = es. index(index=indexname, id=data["id"], body=data)
     return "success"
-
 
 
 # get synonym
@@ -386,31 +424,33 @@ def get_synonym():
     return {"synonyms": lines}
 
 
-
 # delete synonym
 @app.route('/delete_synonym', methods=["POST"])
 def delete_synonym():
-    requestf = request.get_json()                                                         # extraction of request body
-    synonyms = requestf["synonyms"]                                                       # extraction of synonyms which needs to be deleted
+    # extraction of request body
+    requestf = request.get_json()
+    # extraction of synonyms which needs to be deleted
+    synonyms = requestf["synonyms"]
     path = requestf["path"]  # synonym.txt file path
     with open(path, "r") as f:
         lines = f.readlines()
         with open(path, "w") as f:
             for line in lines:
                 # print(line.strip("\n").lower())
-                if line.strip("\n").lower() != synonyms:
+                if line.strip("\n").lower() != synonyms.lower():
                     f.write(line)
-    return {"message":"Successfully Deleted"}
+    return {"message": "Successfully Deleted"}
 
 
 # Delete_by_query
 @app.route("/<indexname>/delete_by_query", methods=['POST'])
 def delete_field_with_query(indexname):
     requestf = request.get_json()                     # extraction of request body
-    querylist = requestf["query"]                     # extraction of query basis which product description needs to be deleted
+    # extraction of query basis which product description needs to be deleted
+    querylist = requestf["query"]
     query = []                                        # initialization of empty list
     for k in querylist:
-        query.append({"term":k})
+        query.append({"term": k})
     body = {
         "query": {
             "bool": {
@@ -418,28 +458,29 @@ def delete_field_with_query(indexname):
             }
         }
     }
-    try:    
+    try:
         res = es.transport.perform_request(
             'POST', '/'+indexname+'/_delete_by_query', body=body)
-        if res["deleted"]==0:
-            return {"message":"Product with the query :"+str(querylist)+" not found"},404
-        return {"message":"Number of products deleted:"+str(res["deleted"]),"response":res}
+        if res["deleted"] == 0:
+            return {"message": "Product with the query :"+str(querylist)+" not found"}, 404
+        return {"message": "Number of products deleted:"+str(res["deleted"]), "response": res}
     except NotFoundError as es1:
-        return es1.info,es1.status_code
+        return es1.info, es1.status_code
 
 
 # Delete Index
 @app.route("/delete_index", methods=['POST'])
 def delete_index():
     requestf = request.get_json()                      # extraction of request body
-    indexname = requestf["indexname"]                  # extraction of indexname from the request body
-    try:    
+    # extraction of indexname from the request body
+    indexname = requestf["indexname"]
+    try:
         res = es.transport.perform_request(
             'DELETE', '/'+indexname)
-        return {"message":"Successfully Deleted!","response":res},"200"
+        return {"message": "Successfully Deleted!", "response": res}, "200"
     except NotFoundError as es1:
-        return es1.info,es1.status_code
-    
+        return es1.info, es1.status_code
+
 
 @app.after_request
 def after_request(response):
